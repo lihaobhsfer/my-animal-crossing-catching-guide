6313803cfd60d490e9f33b7144dc87193c14c924
Hao Li <haoli2@andrew.cmu.edu>
2020-04-07 20:44:34 -0400

    Modify build

---
+++ build/precache-manifest.fcf261397b1ed7c5845e9c3468e8605d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "333adedf8b2f267bc51c907c93f23a3a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "caef1e4dbc50454ffb80",
+    "url": "./static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "eb514eddd38484378c11",
+    "url": "./static/css/main.30745a93.chunk.css"
+  },
+  {
+    "revision": "caef1e4dbc50454ffb80",
+    "url": "./static/js/2.b9e8e3d0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.b9e8e3d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb514eddd38484378c11",
+    "url": "./static/js/main.1fed3498.chunk.js"
+  },
+  {
+    "revision": "2abc9b9fe1c55c0e0757",
+    "url": "./static/js/runtime-main.a37ce646.js"
+  },
+  {
+    "revision": "48393333bb1f3a6fecbb86f33f609189",
+    "url": "./static/media/fish.48393333.csv"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.82306e8441878773d357e5b2aa89df61.js"
+  "./precache-manifest.fcf261397b1ed7c5845e9c3468e8605d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
