32a9fbb7b84c9d43821bb24f79f64b9f56131675
Hao Li <haoli2@andrew.cmu.edu>
2020-04-10 21:15:49 -0400

    Updates

---
+++ precache-manifest.50ae63273599e88392247cabf4a2279f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f277535e48f848d181bb8e13e03e04c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c948b183b72e9f12723a",
+    "url": "./static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "8f657f4927f4dbc49db2",
+    "url": "./static/css/main.568f1a37.chunk.css"
+  },
+  {
+    "revision": "c948b183b72e9f12723a",
+    "url": "./static/js/2.c2558a8c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.c2558a8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f657f4927f4dbc49db2",
+    "url": "./static/js/main.5462daa1.chunk.js"
+  },
+  {
+    "revision": "2abc9b9fe1c55c0e0757",
+    "url": "./static/js/runtime-main.a37ce646.js"
+  },
+  {
+    "revision": "a1a5625f919038e4b7acd15fb0483d2c",
+    "url": "./static/media/data.a1a5625f.csv"
+  }
+]);
